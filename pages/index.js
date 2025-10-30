dd0fb75e463e844e9b5031b1ff7d70af723bfc52
Ahmez18 <b4bitics@gmail.com>
2025-10-30 22:56:13 +1100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'Arial', padding: 40 }}>
+      <h1>SocietySalvor</h1>
+      <p>Turn waste into worth — coming soon!</p>
+    </main>
+  );
+}
